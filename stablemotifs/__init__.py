556b78dcaff598d64845c930206904644c95038a
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-06-04 22:42:13 +0200

    code splitting; basic jupyter integration

--- stablemotifs/__init__.py
+++ stablemotifs/__init__.py
@@ -6,53 +6,16 @@ import subprocess
 import sys
 import tempfile
 
-import pandas as pd
-
 from colomoto_jupyter.io import ensure_localfile
+
+from .jupyter import upload
 
 __version__ = "0.1"
 
 STABLEMOTIFS_JAR = os.path.join(os.path.dirname(__file__), "jars",
         "StableMotifs.jar")
 
-
-class StableMotifsResult(object):
-
-    def __init__(self, wd, model_name, cleanup_wd=False):
-        self.wd = wd
-        self.model_name = model_name
-        self.attractors = self._parse_attractors()
-
-    def _wfile(self, pattern):
-        return os.path.join(self.wd, pattern%self.model_name)
-
-    def _parse_attractors(self):
-        inputf = self._wfile("%s-QuasiAttractors.txt")
-        df = pd.read_csv(inputf, sep='\t', index_col=-1) #index_col=-1 we cheat by making the extra tabs at the end of the rows in the files as the index column, and then resetting it to just natural incrementing integers
-        df.reset_index(inplace=True,drop=True)
-        d = df.to_dict("index")
-        return [d[i] for i in range(len(d))]
-
-    def _parse_control_sets(self):
-        inputf = self._wfile("%s-StableMotifControlSets.txt")
-        control_sets = dict([(i, []) for i in range(len(self.attractors))])
-        with open(inputf) as f:
-            for c in f.readlines():
-                if not c:
-                    continue
-                cc=c.split('\t')[0]
-                cd=[(i.split('=')[0],int(i.split('=')[1])) for i in cc.split(' ')]
-                attr=c.split('\t')[1]
-                attr_id = int(attr[9:])
-                control_sets[attr_id].append(dict(sorted(cd)))
-        return control_sets
-
-    @property
-    def control_sets(self):
-        if not hasattr(self, "_StableMotifsResult__cache_control_sets"):
-            self.__cache_control_sets = self._parse_control_sets()
-        return self.__cache_control_sets
-
+from .results import StableMotifsResult
 
 def load(modelfile, quiet=False):
     """
@@ -60,7 +23,7 @@ def load(modelfile, quiet=False):
 
     :param str modelfile: Filename or URL of Boolean network in BooleanNet format
     :keyword bool quiet: If True, skip computation output
-    :returns: :py:class:`.StableMotifsResult` instance
+    :returns: :py:class:`.results.StableMotifsResult` instance
     """
     modelfile = ensure_localfile(modelfile)
     modelbase = os.path.basename(modelfile)
