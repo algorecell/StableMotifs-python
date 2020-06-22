2c76ef41fd13e186c7107a6fbc2b36517ff6c0ce
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-06-22 15:38:13 +0200

    doc structure

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,8 @@ templates_path = ['_templates']
 exclude_patterns = ["_build"]
 
 
+autodoc_member_order = "groupwise"
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
